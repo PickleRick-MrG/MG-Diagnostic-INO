f34f028d6d8015ff402d0a13b38578d955a85b24
PickleRick-MrG <galviatir@gmail.com>
2024-03-11 16:35:31 +0100

    [FW] EEPROM map and function updated

--- FW/master/eeprom_map.cpp
+++ FW/master/eeprom_map.cpp
@@ -10,6 +10,42 @@
 #include <EEPROM.h>
 #include "Arduino.h"
 
+
+/**
+ * @brief Array which stores the defaults and ranges of acceptable values for
+ * all EEPROM parameters.
+ * @details For every parameter at address A : \n
+ *  + eepromInitTable[A][0] : BT Write Allowed (0 = N / 1 = Y)
+ *  + eepromInitTable[A][1] : Minimum Value
+ *  + eepromInitTable[A][2] : Default Value
+ *  + eepromInitTable[A][3] > Maximum Value
+ * 
+ * @see EEPROM_MAP_ENUM
+*/
+static const uint32_t eepromInitTable[EEPROM_PARAM_MAX][4] = 
+{
+    {0, 0, 0, 0},       // ADD_REV_FW
+    {1, 0, 0, 600},       // ADD_TIME_CURR
+    {1, 0, 120, 600},   // ADD_H2O_TIME
+    {0, 0, 0, 0},       // ADD_ID3
+    {0, 0, 0, 0},       // ADD_ID4
+    {0, 0, 0, 0},       // ADD_ID5
+    {0, 0, 0, 0},       // ADD_ID6
+    {0, 0, 0, 0},       // ADD_ID7
+    {0, 0, 0, 0},       // ADD_ID8
+    {0, 0, 0, 0},       // ADD_ID9
+    {0, 0, 0, 0},       // ADD_ID10
+    {0, 0, 0, 0},       // ADD_ID11
+    {0, 0, 0, 0},       // ADD_ID12
+    {0, 0, 0, 0},       // ADD_ID13
+    {0, 0, 0, 0},       // ADD_ID14
+    {0, 0, 0, 0},       // ADD_ID15
+    {0, 0, 0, 0},       // ADD_ID16
+    {0, 0, 0, 0},       // ADD_ID17
+    {0, 0, 0, 0},       // ADD_ID18
+    {0, 0, 0, 0}        // ADD_ID19
+};
+
 EEPROM_MAP_OBJ::EEPROM_MAP_OBJ()
 {
 }
@@ -20,11 +56,25 @@ EEPROM_MAP_OBJ::~EEPROM_MAP_OBJ()
 
 uint32_t EEPROM_MAP_OBJ::eeprom_get_time()
 {
-    uint32_t time = EEPROM.read(EEPROM_TIME_ADD);
+    uint32_t time = EEPROM.read(ADD_TIME_CURR);
     return time;
 }
 
 void EEPROM_MAP_OBJ::eeprom_set_time(uint32_t time)
 {
-    EEPROM.write(EEPROM_TIME_ADD, time);
+    eeprom_write(ADD_TIME_CURR, time);
+}
+
+void EEPROM_MAP_OBJ::eeprom_write(int add, uint8_t val)
+{
+    if (add >= EEPROM_PARAM_MAX){
+        Serial.println("WNG: EEPROM address out of bounds!");
+    } else {
+        if (val < eepromInitTable[add][1]){
+            val = eepromInitTable[add][1];
+        } else if (val > eepromInitTable[add][3]){
+            val > eepromInitTable[add][3];
+        }
+        EEPROM.write(add, val);
+    }
 }
--- FW/master/eeprom_map.h
+++ FW/master/eeprom_map.h
@@ -11,17 +11,44 @@
 #include <stdint.h>
 #include <string.h>
 
-#define EEPROM_TIME_ADD     1
+/**
+ * @brief EEPROM memory map Addresses
+*/
+enum EEPROM_MAP_ENUM
+{
+    ADD_REV_FW      = 0,    //!< Firmware Revision
+    ADD_TIME_CURR   = 1,    //!< Current TIME
+    ADD_H2O_TIME    = 2,    //!< Maximum Operation TIME for Cooling Water [min]
+    ADD_ID3         = 3,    //!< Empty
+    ADD_ID4         = 4,    //!< Empty
+    ADD_ID5         = 5,    //!< Empty
+    ADD_ID6         = 6,    //!< Empty
+    ADD_ID7         = 7,    //!< Empty
+    ADD_ID8         = 8,    //!< Empty
+    ADD_ID9         = 9,    //!< Empty
+    ADD_ID10        = 10,   //!< Empty
+    ADD_ID11        = 11,   //!< Empty
+    ADD_ID12        = 12,   //!< Empty
+    ADD_ID13        = 13,   //!< Empty
+    ADD_ID14        = 14,   //!< Empty
+    ADD_ID15        = 15,   //!< Empty
+    ADD_ID16        = 16,   //!< Empty
+    ADD_ID17        = 17,   //!< Empty
+    ADD_ID18        = 18,   //!< Empty
+    ADD_ID19        = 19,   //!< Empty
+    EEPROM_PARAM_MAX        //!< Number of EEPROM Parameters
+};
 
 class EEPROM_MAP_OBJ
 {
 private:
-    /* data */
+    static const uint32_t eepromInitTable[EEPROM_PARAM_MAX][4];
 public:
     EEPROM_MAP_OBJ();   // Constructor
     ~EEPROM_MAP_OBJ();  // Destructor
     uint32_t eeprom_get_time();
     void eeprom_set_time(uint32_t time);
+    void eeprom_write(int add, uint8_t val);
 };
 
 #endif // EEPROM_MAP_H
